b4eb5e3c8d7f1b5ec63a0bfc5e9cabc7aee3bea1
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-02-16 22:03:57 -0800

    add shuffle method and call on it upon click

--- src/App.js
+++ src/App.js
@@ -28,18 +28,45 @@ class App extends Component {
       this.setState({
         score: 0,
         clickedCardIds: [],
+        cartoons: this.shuffle(this.state.cartoons)
       });
     } else {
       this.state.clickedCardIds.push(clickedId)
       const score = this.state.score + 1;
       const topScore = this.state.topScore + 1;
       if (score === topScore) {
-        this.setState({ score: score, topScore: topScore })
+        this.setState({ 
+          score: score, 
+          topScore: topScore,
+          cartoons: this.shuffle(this.state.cartoons) 
+          })
       } else {
-        this.setState({ score: score })
+        this.setState({ 
+          score: score, 
+          cartoons: this.shuffle(this.state.cartoons) 
+        })
       }
     }
-  }
+  };
+
+  shuffle = cartoons => {
+    var currentIndex = cartoons.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = cartoons[currentIndex];
+      cartoons[currentIndex] = cartoons[randomIndex];
+      cartoons[randomIndex] = temporaryValue;
+    }
+
+    return cartoons;
+  };
 
   render() {
     return (
