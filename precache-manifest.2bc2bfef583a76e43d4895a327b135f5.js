01b21ff6ecfc6ab1185434e6ebce6e768434894e
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-02-18 21:53:14 -0800

    Updates

---
+++ precache-manifest.2bc2bfef583a76e43d4895a327b135f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db51222f2a57d56b401b12ffe539830b",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "e67ab799ed2b0d494b03",
+    "url": "/memory-game/static/css/main.c4327b69.chunk.css"
+  },
+  {
+    "revision": "6744a597c289eb8ce49a",
+    "url": "/memory-game/static/js/2.6dd9c819.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/memory-game/static/js/2.6dd9c819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67ab799ed2b0d494b03",
+    "url": "/memory-game/static/js/main.088fe85f.chunk.js"
+  },
+  {
+    "revision": "7ba82378826073dce3ea",
+    "url": "/memory-game/static/js/runtime-main.02752f75.js"
+  }
+]);
